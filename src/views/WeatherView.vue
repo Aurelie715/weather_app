8188ef606d68503840b38ea5c4f68ad46d1b1cb9
Aurelie715 <aureliebernard29@gmail.com>
2023-09-26 15:02:11 +0200

    feat: vue installation

---
+++ src/views/WeatherView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+<template>
+    <main>
+        <TheWelcome />
+        <h1>Page meteo</h1>
+    </main>
+</template>
